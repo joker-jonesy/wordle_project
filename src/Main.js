bad9497a92f26a6d12a6b405217cb261eb6718a2
Luke Jones <11872906+joker-jonesy@users.noreply.github.com>
2022-02-16 23:24:59 -0500

    Reupdated to Fix Keyboard

--- src/Main.js
+++ src/Main.js
@@ -1,48 +1,48 @@
 import Guesses from "./components/Guesses";
 import Keys from "./components/Keys";
 import React from "react";
-import { useSelector} from "react-redux";
-// import {SubmitGuess} from "./redux/actions/SubmitGuess";
-// import {DeleteLetter} from "./redux/actions/DeleteLetter";
-// import {InputLetter} from "./redux/actions/InputLetter";
-//
-// const enter_keys = ['Enter', 13];
-// const back_keys = ['Backspace', 8];
+import {useDispatch, useSelector} from "react-redux";
+import {SubmitGuess} from "./redux/actions/SubmitGuess";
+import {DeleteLetter} from "./redux/actions/DeleteLetter";
+import {InputLetter} from "./redux/actions/InputLetter";
 
-// const useEventListener = (eventName, handler, element = window) => {
-//     const savedHandler = React.useRef();
-//
-//     React.useEffect(() => {
-//         savedHandler.current = handler;
-//     }, [handler]);
-//
-//     React.useEffect(() => {
-//         const eventListener = (event) => savedHandler.current(event);
-//         element.addEventListener(eventName, eventListener);
-//         return () => {
-//             element.removeEventListener(eventName, eventListener);
-//         };
-//     }, [eventName, element]);
-// }
+const enter_keys = [13];
+const back_keys = ['Backspace', 8];
+
+const useEventListener = (eventName, handler, element = window) => {
+    const savedHandler = React.useRef();
+
+    React.useEffect(() => {
+        savedHandler.current = handler;
+    }, [handler]);
+
+    React.useEffect(() => {
+        const eventListener = (event) => savedHandler.current(event);
+        element.addEventListener(eventName, eventListener);
+        return () => {
+            element.removeEventListener(eventName, eventListener);
+        };
+    }, [eventName, element]);
+}
 
 function Main() {
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     const win = useSelector(state => state.win);
     const end = useSelector(state => state.end);
     const answer = useSelector(state => state.answer);
-    // const handler = ({key}) => {
-    //     if ('abcdefghijklmnopqrstuvwxyz'.split('').includes(key)) {
-    //         dispatch(InputLetter(key))
-    //     } else if (enter_keys.includes(key)) {
-    //         dispatch(SubmitGuess())
-    //     } else if (back_keys) {
-    //         dispatch(DeleteLetter())
-    //     }
-    //
-    //
-    // };
+    const handler = ({key}) => {
+        if ('abcdefghijklmnopqrstuvwxyz'.split('').includes(key)) {
+            dispatch(InputLetter(key))
+        } else if (enter_keys.includes(key)) {
+            dispatch(SubmitGuess())
+        } else if (back_keys) {
+            dispatch(DeleteLetter())
+        }
 
-    // useEventListener("keydown", handler);
+
+    };
+
+    useEventListener("keydown", handler);
 
     const reload_page= ()=>{
         window.location.reload(false);
