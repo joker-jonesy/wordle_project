6aadc734116f9b948b84d373416f22880bad748d
Luke Jones <11872906+joker-jonesy@users.noreply.github.com>
2022-02-17 12:29:23 -0500

    Fixed Issue on Keyboard Coloring

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,48 +1,52 @@
-import {useDispatch,useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {InputLetter} from "../redux/actions/InputLetter";
 
-function Key(props){
-    const guesses=useSelector(state=>state.guesses);
-    const answer=useSelector(state=>state.answer);
-    const cTry= useSelector(state=>state.try);
-    const guessed=useSelector(state=>state.guessed);
+function Key(props) {
+    const guesses = useSelector(state => state.guesses);
+    const answer = useSelector(state => state.answer);
+    const cTry = useSelector(state => state.try);
+    const guessed = useSelector(state => state.guessed);
 
-    const dispatch=useDispatch();
+    const dispatch = useDispatch();
 
     let style;
-    let stop=false;
+    let stop = false;
 
-    if(guessed.includes(props.vl)){
-        if(answer.join('').includes(props.vl)){
-            for(let i=0; i<cTry; i++){
-                if(!stop){
-                    if(guesses[i].indexOf(props.vl)===answer.join('').indexOf(props.vl)){
-                        style={
-                            color:"white",
-                            backgroundColor:"green"
+    if (guessed.includes(props.vl)) {
+        if (answer.join('').includes(props.vl)) {
+            for (let i = 0; i < cTry; i++) {
+                console.log(props.vl, guesses[i].indexOf(props.vl), answer.indexOf(props.vl));
+                if (!stop) {
+                    if (guesses[i].indexOf(props.vl) === answer.indexOf(props.vl)) {
+                        style = {
+                            color: "white",
+                            backgroundColor: "green"
                         }
-                        stop=true;
-                    }else{
-                        style={
-                            color:"black",
-                            backgroundColor:"yellow"
+
+                        stop = true;
+                        
+
+                    } else {
+                        style = {
+                            color: "black",
+                            backgroundColor: "yellow"
                         }
-                        stop=true;
                     }
                 }
 
             }
 
-        }else{
-            style={
-                color:"white",
-                backgroundColor:"black"
+        } else {
+            style = {
+                color: "white",
+                backgroundColor: "black"
             }
         }
     }
 
     return (
-        <div className={"key"} style={style} onClick={()=>dispatch(InputLetter(props.vl))}>{props.vl.toUpperCase()}</div>
+        <div className={"key"} style={style}
+             onClick={() => dispatch(InputLetter(props.vl))}>{props.vl.toUpperCase()}</div>
     )
 }
 
